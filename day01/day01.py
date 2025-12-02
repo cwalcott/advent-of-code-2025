acc5f5a62b14d705fe3a2bb1244965ca5deea741
Costa Walcott <cwalcott@gmail.com>
2025-12-01 22:48:17 -0500

    Day 1 part 2

--- day01/day01.py
+++ day01/day01.py
@@ -1,23 +1,53 @@
 def day01(rotations: list[str]) -> int:
-    dial, zero_count = 50, 0
+    dial, count = 50, 0
 
     for rotation in rotations:
         direction, number = rotation[0:1], int(rotation[1:])
         match direction:
             case 'L':
-                dial = (dial + number) % 100
+                dial -= number
             case 'R':
-                dial = (dial - number) % 100
+                dial += number
             case _:
                 raise f'Invalid direction ${direction}'
 
-        if dial == 0:
-            zero_count += 1
+        dial %= 100
 
-    return zero_count
+        if dial == 0:
+            count += 1
+
+    return count
+
+
+def day01_434C49434B(rotations: list[str]) -> int:
+    dial, count = 50, 0
+
+    for rotation in rotations:
+        direction, number = rotation[0:1], int(rotation[1:])
+        match direction:
+            case 'L':
+                while number > 0:
+                    dial -= 1
+                    number -= 1
+                    if dial == 0:
+                        count += 1
+                    elif dial == -1:
+                        dial = 99
+            case 'R':
+                while number > 0:
+                    dial += 1
+                    number -= 1
+                    if dial == 100:
+                        count += 1
+                        dial = 0
+            case _:
+                raise f'Invalid direction ${direction}'
+
+    return count
 
 
 if __name__ == "__main__":
     with open('input', 'r') as f:
-        lines = f.readlines()
+        lines = [line.strip() for line in f]
         print(day01(lines))
+        print(day01_434C49434B(lines))
--- day01/day01_test.py
+++ day01/day01_test.py
@@ -1,11 +1,14 @@
 import unittest
 
-from day01.day01 import day01
+from day01.day01 import day01, day01_434C49434B
 
 
 class Day01TestCase(unittest.TestCase):
     def test(self):
-        self.assertEqual(3, day01(['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82', ]))
+        self.assertEqual(3, day01(['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82']))
+
+    def test_434C49434B(self):
+        self.assertEqual(6, day01_434C49434B(['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82']))
 
 
 if __name__ == '__main__':
