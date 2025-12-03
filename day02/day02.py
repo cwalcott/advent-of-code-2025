92084d96304ff2e106d408178d7759bffaac94ff
Costa Walcott <cwalcott@gmail.com>
2025-12-02 21:21:19 -0500

    Day 2 Part 2

--- day02/day02.py
+++ day02/day02.py
@@ -11,6 +11,23 @@ def day02(ranges_str: str) -> int:
     return result
 
 
+def day02_part2(ranges_str: str) -> int:
+    result = 0
+    for r in ranges_str.split(','):
+        start_str, end_str = r.split('-')
+        start, end = int(start_str), int(end_str)
+        for i in range(start, end + 1):
+            i_str = str(i)
+            for i_end in range(1, (len(i_str) // 2) + 1):
+                if i_str[0:i_end] * (len(i_str) // i_end) == i_str:
+                    result += i
+                    break
+
+    return result
+
+
 if __name__ == "__main__":
     with open('input', 'r') as f:
-        print(day02(f.read()))
+        r = f.read()
+        print(day02(r))
+        print(day02_part2(r))
