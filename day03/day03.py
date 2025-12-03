bbd049891732505579a2035eb12dbc73f13071e0
Costa Walcott <cwalcott@gmail.com>
2025-12-03 18:19:31 -0500

    Day 3 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -1,3 +1,4 @@
+import heapq
 
 
 def largest_joltage(bank: str) -> int:
@@ -22,10 +23,43 @@ def largest_joltage(bank: str) -> int:
     return digits[0] * 10 + digits[1]
 
 
+def largest_joltage_part2(bank: str) -> int:
+    bank_numbers = [int(digit) for digit in bank]
+    digits = set()
+    for i in range(len(bank_numbers)):
+        digits.add(i)
+        if len(digits) > 12:
+            last, last_idx = None, None
+            for j in range(i + 1):
+                if j in digits:
+                    if last is not None and bank_numbers[j] > last:
+                        digits.remove(last_idx)
+                        break
+                    else:
+                        last, last_idx = bank_numbers[j], j
+
+            if len(digits) > 12:
+                digits.remove(i)
+
+    result = 0
+    for i in range(len(bank)):
+        if i in digits:
+            result *= 10
+            result += int(bank[i])
+    return result
+
+
 def day03(banks: list[str]) -> int:
     result = 0
     for bank in banks:
         result += largest_joltage(bank)
+
+    return result
+
+def day03_part2(banks: list[str]) -> int:
+    result = 0
+    for bank in banks:
+        result += largest_joltage_part2(bank)
 
     return result
 
@@ -34,3 +68,4 @@ if __name__ == "__main__":
     with open('input', 'r') as f:
         lines = [line.strip() for line in f]
         print(day03(lines))
+        print(day03_part2(lines))
--- day03/day03_test.py
+++ day03/day03_test.py
@@ -1,17 +1,26 @@
 import unittest
 
-from day03.day03 import largest_joltage, day03
+from day03.day03 import largest_joltage, day03, largest_joltage_part2
 
 
 class Day03TestCase(unittest.TestCase):
     def test_joltage1(self):
         self.assertEqual(98, largest_joltage('987654321111111'))
 
+    def test_joltage1_part2(self):
+        self.assertEqual(987654321111, largest_joltage_part2('987654321111111'))
+
     def test_joltage2(self):
         self.assertEqual(89, largest_joltage('811111111111119'))
 
+    def test_joltage2_part2(self):
+        self.assertEqual(811111111119, largest_joltage_part2('811111111111119'))
+
     def test_joltage3(self):
         self.assertEqual(78, largest_joltage('234234234234278'))
+
+    def test_joltage3_part2(self):
+        self.assertEqual(434234234278, largest_joltage_part2('234234234234278'))
 
     def test_joltage4(self):
         self.assertEqual(92, largest_joltage('818181911112111'))
