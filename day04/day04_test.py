b1827c22fa6ff1d3dc474e1f8ea0845192bdd6bb
Costa Walcott <cwalcott@gmail.com>
2025-12-04 20:53:37 -0500

    Day 4 part 2

--- day04/day04_test.py
+++ day04/day04_test.py
@@ -1,11 +1,11 @@
 import unittest
 
-from day04.day04 import day04
+from day04.day04 import day04, day04_part2
 
 
 class Day04TestCase(unittest.TestCase):
-    def test_sample(self):
-        rolls = [
+    def setUp(self):
+        self.rolls = [
             '..@@.@@@@.',
             '@@@.@.@.@@',
             '@@@@@.@.@@',
@@ -17,7 +17,12 @@ class Day04TestCase(unittest.TestCase):
             '.@@@@@@@@.',
             '@.@.@@@.@.'
         ]
-        self.assertEqual(13, day04(rolls))
+
+    def test_sample(self):
+        self.assertEqual(13, day04(self.rolls))
+
+    def test_sample_part2(self):
+        self.assertEqual(43, day04_part2(self.rolls))
 
 
 if __name__ == '__main__':
