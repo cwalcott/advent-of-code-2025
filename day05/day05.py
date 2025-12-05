f149f21f1a3df8f5a922c93eb8676f679b2313a1
Costa Walcott <cwalcott@gmail.com>
2025-12-05 15:03:48 -0500

    Day 5 part 2

--- day05/day05.py
+++ day05/day05.py
@@ -7,11 +7,36 @@ def day05(lines: list[str]) -> int:
             start, end = line.split('-')
             ranges.append((int(start), int(end)))
         elif line:
-            id = int(line)
+            identifier = int(line)
             for start, end in ranges:
-                if start <= id <= end:
+                if start <= identifier <= end:
                     count += 1
                     break
+
+    return count
+
+
+def merge_ranges(unmerged: list[tuple]) -> list[tuple[int, int]]:
+    result = []
+    unmerged.sort()
+
+    for start, end in unmerged:
+        if not result or result[-1][1] < start:
+            result.append((start, end))
+        else:
+            result[-1] = (min(result[-1][0], start), max(result[-1][1], end))
+
+    return result
+
+
+def day05_part2(lines: list[str]) -> int:
+    unmerged = [tuple(map(int, line.split('-'))) for line in lines if '-' in line]
+
+    ranges = merge_ranges(unmerged)
+
+    count = 0
+    for range in ranges:
+        count += range[1] - range[0] + 1
 
     return count
 
@@ -20,3 +45,4 @@ if __name__ == "__main__":
     with open('input', 'r') as f:
         lines = [line.strip() for line in f]
         print(day05(lines))
+        print(day05_part2(lines))
--- day05/day05_test.py
+++ day05/day05_test.py
@@ -1,12 +1,20 @@
 import unittest
 
-from day05.day05 import day05
+from day05.day05 import day05, day05_part2, merge_ranges
 
 
 class Day05TestCase(unittest.TestCase):
+    def setUp(self):
+        self.lines = ['3-5', '10-14', '16-20', '12-18', '', '1', '5', '8', '11', '17', '32']
+
     def test(self):
-        lines = ['3-5', '10-14', '16-20', '12-18', '', '1', '5', '8', '11', '17', '32']
-        self.assertEqual(3, day05(lines))
+        self.assertEqual(3, day05(self.lines))
+
+    def test_part2(self):
+        self.assertEqual(14, day05_part2(self.lines))
+
+    def test_merge_ranges(self):
+        self.assertEqual([(3, 5), (10, 20)], merge_ranges([(3, 5), (10, 14), (16, 20), (12, 18)]))
 
 
 if __name__ == '__main__':
